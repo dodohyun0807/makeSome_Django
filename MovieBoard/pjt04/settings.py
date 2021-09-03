436c75109e2a4bba45552a225860e9875699b32c
dohyun <dodohyun0807@gmail.com>
2021-09-03 20:31:39 +0900

    0903

--- MovieBoard/pjt04/settings.py
+++ MovieBoard/pjt04/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!b6!wb75x8)imhh&&mi!y08!_e9-(3+6q5p-70j7tj2m9s+1lc'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
