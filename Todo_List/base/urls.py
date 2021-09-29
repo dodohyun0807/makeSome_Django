4d34b064e531ce23193361890052e229d5103d73
dohyun <dodohyun0807@gmail.com>
2021-09-29 21:41:31 +0900

    ToDoList

---
+++ Todo_List/base/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
+from django.contrib.auth.views import LogoutView
+
+app_name = 'base'
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='base:login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
+]
