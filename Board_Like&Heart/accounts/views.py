65f010753f4cbbe192dc0948ffac0897303f682a
dohyun <dodohyun0807@gmail.com>
2021-11-02 19:16:09 +0900

    1102

---
+++ Board_Like&Heart/accounts/views.py
@@ -0,0 +1,139 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+# from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.views.decorators.http import require_POST, require_http_methods
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash, get_user_model
+from django.contrib.auth.forms import (
+    AuthenticationForm, 
+    UserCreationForm, 
+    PasswordChangeForm,
+)
+from django.http import JsonResponse
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+# Create your views here.
+@require_http_methods(['GET', 'POST'])
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        # form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+@require_POST
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect('articles:index')
+
+
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+@require_POST
+def delete(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+        auth_logout(request)
+    return redirect('articles:index')
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        # form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('articles:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
+
+
+def profile(request, username):
+    person = get_object_or_404(get_user_model(), username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+    
+    
+@require_POST
+def follow(request, user_pk):
+    if request.user.is_authenticated:
+        # 팔로우 받는 사람
+        you = get_object_or_404(get_user_model(), pk=user_pk)
+        me = request.user
+
+        # 나 자신은 팔로우 할 수 없다.
+        if you != me:
+            if you.followers.filter(pk=me.pk).exists():
+            # if request.user in person.followers.all():
+                # 팔로우 끊음
+                you.followers.remove(me)
+                isFollowed = False
+            else:
+                # 팔로우 신청
+                you.followers.add(me)
+                isFollowed = True
+        
+        context = {
+            'isFollowed' : isFollowed,
+            'followers' : you.followers.count(),
+            'followings' : you.followings.count(),
+        }
+
+        return JsonResponse(context)
+    return redirect('accounts:login')
